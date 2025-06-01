00787d4a59dff1e165c2794a96a0bfb6d410cfff
Claudia Orostica <136994710+Clorostica@users.noreply.github.com>
2025-06-01 08:11:24 +0200

    Update map_module.js

--- JS/map_module.js
+++ JS/map_module.js
@@ -1,12 +1,28 @@
-const toggleBtn = document.getElementById('toggleTheme');
-const body = document.body;
+import createGlobe from 'https://cdn.skypack.dev/cobe'
 
-toggleBtn.addEventListener('click', () => {
-  body.classList.toggle('isLightMode');
+let phi = 0
+let canvas = document.getElementById("cobe")
 
-  if (body.classList.contains('isLightMode')) {
-    toggleBtn.textContent = '☀️'; // 
-  } else {
-    toggleBtn.textContent = '🌙';
-  }
-});
+const globe = createGlobe(canvas, {
+  devicePixelRatio: 1,
+  width: 350,
+  height: 350,
+  phi: 0,
+  theta: 0.7,
+  dark: 1,
+  diffuse: 1,
+  scale: 1,
+  mapSamples: 26000,
+  mapBrightness: 8,
+  baseColor: [1, 1, 1],
+  markerColor: [255, 0, 0],
+  glowColor: [1, 1, 1],
+  offset: [0, 0],
+  markers: [
+    { location: [52.52, 13.405], size: 0.04 }, 
+  ],
+  onRender: (state) => {
+    state.phi = phi
+    phi += 0.008
+  },
+})
