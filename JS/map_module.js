d358cc27b920606a08338cd3702e92467b57aea2
Claudia Orostica <136994710+Clorostica@users.noreply.github.com>
2025-06-01 07:59:21 +0200

    Update map_module.js

--- JS/map_module.js
+++ JS/map_module.js
@@ -1,1 +1,12 @@
+const toggleBtn = document.getElementById('toggleTheme');
+const body = document.body;
 
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('isLightMode');
+
+  if (body.classList.contains('isLightMode')) {
+    toggleBtn.textContent = '☀️'; // 
+  } else {
+    toggleBtn.textContent = '🌙';
+  }
+});
