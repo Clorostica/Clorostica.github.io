1e740f6b1dfb77db1e7a91dec8f671199e2d24a3
Claudia Orostica <136994710+Clorostica@users.noreply.github.com>
2025-07-17 11:16:00 +0200

    Update lazyImages.js

--- JS/lazyImages.js
+++ JS/lazyImages.js
@@ -1,14 +1,1 @@
-const lazyImages = document.querySelectorAll('img.lazy-img');
 
-const observer = new IntersectionObserver((entries, obs) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const img = entry.target;
-      img.src = img.dataset.src;
-      img.classList.remove('lazy-img');
-      obs.unobserve(img);
-    }
-  });
-});
-
-lazyImages.forEach(img => observer.observe(img));
