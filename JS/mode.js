4bd256271350b8f54073531a991c9962f870cb5b
Claudia Orostica <136994710+Clorostica@users.noreply.github.com>
2025-10-13 20:53:49 +0200

    Update mode.js

--- JS/mode.js
+++ JS/mode.js
@@ -1,22 +1,22 @@
-const toggleBtn = document.getElementById('toggleTheme');
-const body = document.body;
 
-  window.addEventListener('DOMContentLoaded', () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'light') {
-      body.classList.add('isLightMode');
-      toggleBtn.textContent = '🌙';
-    } else {
-      body.classList.remove('isLightMode');
-      toggleBtn.textContent = '☀️'; 
-    }
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('toggleTheme');
+  const body = document.body;
+
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'light') {
+    body.classList.add('isLightMode');
+    toggleBtn.textContent = '🌙';
+  } else {
+    body.classList.remove('isLightMode');
+    toggleBtn.textContent = '☀️';
+  }
 
   toggleBtn.addEventListener('click', () => {
     body.classList.toggle('isLightMode');
-
     const isLight = body.classList.contains('isLightMode');
     toggleBtn.textContent = isLight ? '🌙' : '☀️';
     localStorage.setItem('theme', isLight ? 'light' : 'dark');
   });
+});
 
